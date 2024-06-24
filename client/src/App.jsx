a33b24ee1556a5b57c6cd2d926b48e5dbb40e819
Deco <cedricktiako@coknens.com>
2024-06-24 06:05:02 +0200

    ajont des differentes police de caractere et creation des pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 export default function App() {
   return (
-    <div className='deco text-3xl text-purple-800 ' >deco</div>
+    <BrowserRouter className='deco text-3xl text-purple-800 ' >
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/project' element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
