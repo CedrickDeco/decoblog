ed57ca8447ddb0cb4dde3eea26161410a1108b2f
Deco <cedricktiako@coknens.com>
2024-06-27 08:56:34 +0200

    connexion à la base de donnée

--- api/server.js
+++ api/server.js
@@ -1,9 +1,14 @@
 const express = require('express')
 require('dotenv').config({ path: './api/config/.env' })
+const mongoose = require('mongoose')
+const connectDb = require('./config/db.js')
 const port = process.env.PORT
 
 const app = express()
 
+
+//Database
+connectDb()
 
 //Start the server
 app.listen(4000, () => {
